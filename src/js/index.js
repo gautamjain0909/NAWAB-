9248b9837fcf3c8331f1f9aca5d58ef1c486c8d3
Gautam Jain <gautamjain0909@gmail.com>
2024-04-20 01:09:18 +0530

    Minor changes

--- src/js/index.js
+++ src/js/index.js
@@ -19,14 +19,3 @@ Promise.all([preloadImages(".tiles__line-img"), preloadFonts("rmd7deq")]).then(
     backtopEl.addEventListener("click", () => scroll.scrollTo(headerEl));
   }
 );
-
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function () {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-50px";
-  }
-  prevScrollpos = currentScrollPos;
-};
